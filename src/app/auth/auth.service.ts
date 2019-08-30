3089c92c86aceece3be9173067507d571dfc33aa
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2019-08-30 15:29:06 +0530

    login stores on localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,10 +32,11 @@ export class AuthService {
 
   login(roll: string, password: string) {
     const authData = {roll, password};
-    this.http.post<{token: string}>('http://localhost:3000/api/login', authData)
+    this.http.post<{token: string, id: string}>('http://localhost:3000/api/login', authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
+        localStorage.setItem('_id', response.id);
       });
   }
 }

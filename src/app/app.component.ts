22e356d44de6ad8269a0451737fec51912e30503
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2019-08-31 23:49:29 +0530

    live notifcations!!!!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { LadderService } from './app.service';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,17 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'ladder-ranking';
+  private notif: Subscription;
+  public id: string;
 
-  constructor( private authService: AuthService) {}
+  constructor( private authService: AuthService, private ladderService: LadderService) {}
 
   ngOnInit() {
     this.authService.autoAuthUser();
+    this.id = localStorage.getItem('_id');
+    this.notif = timer(5000)
+      .subscribe(data => {
+        this.ladderService.getNumberChallenge(this.id);
+      });
   }
 }

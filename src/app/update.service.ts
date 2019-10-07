7cdf052d320f931631cca16716f3688696cd8767
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2019-10-07 16:31:18 +0530

    update updated

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material';
 import { SwUpdate } from '@angular/service-worker';
-import { duration } from 'moment';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class UpdateService {
   constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
     this.swUpdate.available.subscribe(evt => {
       const snack = this.snackbar.open('Update Available', 'Reload', { duration: 6000 });
-
       snack
         .onAction()
         .subscribe(() => {
